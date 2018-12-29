4884cf5a84864efdc3c1aba37e43e85155204ac8
jin <jinlu.hansome@163.com>
2018-12-29 19:45:39 +0800

    测试idea

---
+++ readme.txt
@@ -0,0 +1,1 @@
+访问 http://localhost:8080/#/folder_tree 测试
--- src/lib/util.js
+++ src/lib/util.js
@@ -13,6 +13,7 @@ export const getToken = (tokenName = 'token') => {
   return Cookies.get(tokenName)
 }
 
+// 1.先把文件对象放入对应的文件夹,此时还是扁平数组解构
 export const putFileInFolder = (folderList, fileList) => {
   const folderListCloned = clonedeep(folderList)
   const fileListCloned = clonedeep(fileList)
@@ -33,15 +34,16 @@ export const putFileInFolder = (folderList, fileList) => {
   })
 }
 
+// 2.将上面的文件夹重新递归排列，形成目录树结构
 export const transferFolderToTree = folderList => {
   if (!folderList.length) return []
   const folderListCloned = clonedeep(folderList)
   const handle = id => {
-    let arr = []
+    let arr = []  // 保存处理完的目录树
     folderListCloned.forEach(folder => {
       if (folder.folder_id === id) {
-        const children = handle(folder.id)
-        if (folder.children) folder.children = [].concat(folder.children, children)
+        const children = handle(folder.id)  // 递归调用处理目录
+        if (folder.children) folder.children = [].concat(folder.children, children)  // 将子目录，子文件拼接在一起
         else folder.children = children
         folder.title = folder.name
         arr.push(folder)
